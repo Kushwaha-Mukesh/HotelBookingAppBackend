0f9a4deeff6281bffa14aa0bbb93a9e1ac94b459
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-03-04 20:23:54 +0545

    setup cloudinary and added hotel schema

---
+++ src/routes/my-hotels.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5MB
+  },
+});
+
+router.post(
+  "/",
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel = req.body;
+
+      // upload the images to cloudinary
+      const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        const res = await cloudinary.v2.uploader.upload(dataURI);
+        return res.url;
+      });
+
+      const imageUrls = await Promise.all(uploadPromises);
+
+      // if upload is successful, add the urls to the new hotel
+    } catch (error) {
+      console.log("Error creating hotel: ", error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
