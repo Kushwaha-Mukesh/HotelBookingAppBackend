ce48aa265a0ce1392c9a6c8eb06a994bd98a4fdf
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-03-22 18:22:31 +0545

    build for deployment

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,8 @@ cloudinary.config({
 
 mongoose
   .connect(process.env.DB_URL as string)
-  .then(() => {
-    console.log("Database connection established");
-  })
-  .catch((error) => {
-    console.log("Error connecting to database");
-    console.log(error);
+  .then(() => {})
+  .catch(() => {
     process.exit(1);
   });
 
