746332803e7b35a528a460023780b4690054b072
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-03-23 17:09:17 +0545

    changes cors policy

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
     credentials: true,
   })
